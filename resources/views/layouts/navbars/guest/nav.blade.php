0bd1f7a15b8226f60058b075ba3dc6a527b52c2a
Bryan <reizcorps@gmail.com>
2023-05-29 08:24:04 +0700

    Bug Fix

--- resources/views/layouts/navbars/guest/nav.blade.php
+++ resources/views/layouts/navbars/guest/nav.blade.php
@@ -4,13 +4,6 @@
     <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 {{ (Request::is('static-sign-up') ? 'text-white' : '') }}" href="{{ url('dashboard') }}">
       Komunitas Radio Baraya Bandung
     </a>
-    <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon mt-2">
-        <span class="navbar-toggler-bar bar1"></span>
-        <span class="navbar-toggler-bar bar2"></span>
-        <span class="navbar-toggler-bar bar3"></span>
-      </span>
-    </button>
     <div class="collapse navbar-collapse" id="navigation">
       <ul class="navbar-nav mx-auto">
         @if (auth()->user())
